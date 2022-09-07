3459fb58afc6e3fbc4191b405b5595ca2ed7f68a
SabaBeradze <113034681+SabaBeradze@users.noreply.github.com>
2022-09-07 17:36:35 +0400

    Add files via upload

---
+++ Abstract 3D Geometry/src/fop/w7geo/Square.java
@@ -0,0 +1,38 @@
+package fop.w7geo;
+
+public class Square extends BaseArea {
+    private double length;
+
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public double circumference() {
+        return 4 * length;
+    }
+
+    @Override
+    public double area() {
+        return length * length;
+    }
+
+    @Override
+    public boolean isSquare() {
+        return true;
+    }
+
+    @Override
+    public Square toSquare() {
+        return this;
+    }
+
+    public Square(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" + "length=" + length + "; " + super.toString() + '}';
+    }
+}
